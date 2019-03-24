f28abf9c264d7367fb279154014a046a8728722d
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-24 22:51:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
-class Student 
+class Student
   attr_accessor :student
 end
 
-def "Student" do 
+def "Student" do
   let!(:student) { Student.new }
 end
 
 def returns_a_greeting_phrase
-  puts "Hey there! I'm so excited to learn stuff"  
+  puts "Hey there! I'm so excited to learn stuff"
 end
